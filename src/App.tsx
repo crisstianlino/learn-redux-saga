d53da41aed6c17d4e521d133c93a53b107a2acc3
Cristian Lino <cristianlino11drig@gmail.com>
2023-04-17 15:35:29 -0300

    init react-app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div
+            style={{ width: '100%', height: '300px', border: '1px solid blue' }}
+        ></div>
+    )
+}
+
+export default App
